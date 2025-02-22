da582bcbdb43bcd6274afe9eb35a9d782150ebbe
samoilovmisha <camoilovmisha0@gmail.com>
2025-02-22 14:44:45 +0300

    220225

--- RXTX/RX/RX.ino
+++ RXTX/RX/RX.ino
@@ -4,8 +4,11 @@ struct MyData {
   bool status;
 };
 
+int m_id{}; float m_value{}; bool m_status{};
+
 MyData receivedData;
 void setup() {
+  pinMode(13, OUTPUT);
   Serial.begin(9600);
 }
 
@@ -20,4 +23,19 @@ void loop() {
     Serial.print("Status:  ");
       Serial.println(receivedData.status);
   }
+  m_id = receivedData.id ;
+  m_value = receivedData.value;
+  m_status = receivedData.status;
+
+  if(receivedData.id == m_id)
+  {
+    if(receivedData.value == m_value)
+    {
+      if(receivedData.status == m_status)
+      {
+        digitalWrite(13, HIGH);
+      }
+    }
+  }
+
 }
